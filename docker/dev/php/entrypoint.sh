f9a66cb22c6c9879d8698da410947efbbd8035a8
amir <a.shadanfar.it@gmail.com>
2021-04-05 02:22:58 +0300

    config predis & queue name in docker

--- docker/dev/php/entrypoint.sh
+++ docker/dev/php/entrypoint.sh
@@ -13,7 +13,8 @@ chmod -R 777 /www/storage /www/bootstrap
 
 # Starting The Queues
 nohup php artisan queue:work --tries=3 --timeout=120> /dev/null &
-nohup php artisan queue:work --queue=expiration --tries=3 --timeout=60 > /dev/null &
+nohup php artisan queue:work --queue=subscription --tries=3 --timeout=60 > /dev/null &
+nohup php artisan queue:work --queue=registration --tries=3 --timeout=60 > /dev/null &
 nohup cron -f &
 nohup php-fpm &
 
